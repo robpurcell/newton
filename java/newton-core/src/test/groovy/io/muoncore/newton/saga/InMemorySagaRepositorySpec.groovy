315006c3041cbde99be11b6d669d8f427650cf0b
Rob Purcell <rob.purcell@gmail.com>
2017-11-20 19:05:47 +0000

    Add more unit tests for the InMemorySagaRepository

--- java/newton-core/src/test/groovy/io/muoncore/newton/saga/InMemorySagaRepositorySpec.groovy
+++ java/newton-core/src/test/groovy/io/muoncore/newton/saga/InMemorySagaRepositorySpec.groovy
@@ -1,5 +1,9 @@
 package io.muoncore.newton.saga
 
+import static org.hamcrest.Matchers.hasItem
+import static org.hamcrest.Matchers.hasSize
+import static spock.util.matcher.HamcrestSupport.that
+
 import io.muoncore.newton.InMemoryTestConfiguration
 import io.muoncore.newton.NewtonEvent
 import org.apache.commons.lang3.RandomStringUtils
@@ -40,9 +44,28 @@ class InMemorySagaRepositorySpec extends Specification {
 
     then:
     def expectedSagaCreated = new SagaCreated(aTestSaga.getClass().name, eventId, sagaId)
-    repository.getSagaCreatedStore().size() == 1
     repository.getSagaCreatedStore().get(eventId).size() == 1
     repository.getSagaCreatedStore().get(eventId).get(0) == expectedSagaCreated
+  }
+
+
+  def "Saving a new Saga updates creation by the triggering Event"() {
+    given:
+    String sagaId = TestDataGenerators.aString()
+    String anotherSagaId = TestDataGenerators.aString()
+    String eventId = TestDataGenerators.aString()
+    Saga aTestSaga = new SimpleSaga(id: sagaId)
+    Saga anotherTestSaga = new SimpleSaga(id: anotherSagaId)
+    NewtonEvent<String> event = new TriggerASimpleSagaEvent(id: eventId)
+
+    when:
+    repository.saveNewSaga(aTestSaga, event)
+    repository.saveNewSaga(anotherTestSaga, event)
+
+    then:
+    def expectedSagaCreated = new SagaCreated(aTestSaga.getClass().name, eventId, sagaId)
+    that repository.getSagaCreatedStore().get(eventId), hasSize(2)
+    that repository.getSagaCreatedStore().get(eventId), hasItem(expectedSagaCreated)
   }
 
   private class SimpleSaga extends StatefulSaga {
